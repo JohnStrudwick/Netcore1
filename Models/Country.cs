ad34a27218602b0e99879721eb6e702d0c27d28f
John Strudwick <john.strudwick@wyldwind.com>
2016-03-16 13:05:47 +0000

    registers1

---
+++ Models/Country.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication5
+{
+	
+	// {"serial-number":6,"hash":"2778fa2a0a97b98728053b4caf7fee918aa0357c",
+	// "entry":{"citizen-names":"Briton;British citizen",
+	// "country":"GB",
+	// "name":"United Kingdom","official-name":"The United Kingdom of Great Britain and Northern Ireland"}}
+	public class Country
+	{
+		public int serialnumber { get; set; }
+		public string hash { get; set; }
+		public Entry entry { get; set; }
+	}
+
+	public class Entry
+	{
+		public string citizen_names { get; set; }
+		public string country { get; set; }
+		public string name { get; set; }
+		public string official_name { get; set; }
+	}
+
+
+}
